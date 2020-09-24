38fe4aa85f24baaa66f5d9e76423f575498d4076
ikoan0808 <71825883+ikoan0808@users.noreply.github.com>
2020-09-24 22:05:00 +0900

    ho

--- discordbot.py
+++ discordbot.py
@@ -16,7 +16,98 @@ Hololive = {
         "ときのそら",
         "https://yt3.ggpht.com/a/AATXAJzGvZJuJ92qM5WcfBcDZqPFSj_CGIEYp9VFmA=s288-c-k-c0xffffffff-no-rj-mo"
     ],
-    
+    "UC-hM6YJuNYVAmUWxeIr9FeA": [
+        "さくらみこ",
+        "https://yt3.ggpht.com/a/AATXAJyKGBusDmf7stiwXDVORLo21xtnBj2YUHrPd-CxYA=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCdn5BQ06XqgXoAxIhbqw5Rg": [
+        "白上フブキ",
+        "https://yt3.ggpht.com/a/AATXAJztvRXzNX8UtsaUZfLLBh32VBBwNPcvy_TklUjnWA=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCQ0UDLQCjY0rmuxCDE38FGg": [
+        "夏色まつり ",
+        "https://yt3.ggpht.com/a/AATXAJw8RXWyEofFZFI5EwEb7lXDq3Cm6l0SThQxT9XG=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCD8HOxPs4Xvsm8H0ZxXGiBw": [
+        "夜空メル",
+        "https://yt3.ggpht.com/a/AATXAJxGYXGi2fgjHoYCSuqLzsh6p-CrlNvRTRFmZmK_=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UC1CfXB_kRs3C-zaeTG3oGyg": [
+        "赤井はあと",
+        "https://yt3.ggpht.com/a/AATXAJw3IoOJZlLAn_Eyy0r6gJHR3vt64XHfuXCailaCPw=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCFTLzh12_nrtzqBPsTCqenA": [
+        "アキロゼ",
+        "https://yt3.ggpht.com/a/AATXAJz-Y3lEysu2wkT7AdTaPU77l938jwf2p2r8hi7e=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UC1opHUrw8rvnsadT-iGp7Cg": [
+        "湊あくあ",
+        "https://yt3.ggpht.com/a/AATXAJxXeCPuSGxq-_NfKX4WSpCf7FtnOkPNzHd2s9Sh9g=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UC1suqwovbL1kzsoaZgFZLKg": [
+        "癒月ちょこ",
+        "https://yt3.ggpht.com/a/AATXAJyy9e7DUSSfuWdROtr9oJxho6jVqSDhGdsRESDU=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UC7fk0CB07ly8oSl0aqKkqFg": [
+        "百鬼あやめ",
+        "https://yt3.ggpht.com/a/AATXAJybvEUdsgDTZasoeAgVPHwDcWzmfLdngzWFcLx7=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCXTpFs_3PqI41qX2d9tL2Rw": [
+        "紫咲シオン",
+        "https://yt3.ggpht.com/a/AATXAJx2dQl8H1TG2HBy9bMCMP-nHxOVXALwAJz8MFS_rA=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCvzGlP9oQwU--Y0r9id_jnA": [
+        "大空スバル",
+        "https://yt3.ggpht.com/a/AATXAJx_CMc_Ne4b93d6Hr3nMSqLbijvq6UxzPO8apsk=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCp-5t9SrOQwXMU7iIjQfARg": [
+        "大神ミオ",
+        "https://yt3.ggpht.com/a/AATXAJzFpyPB7LFGFq5DIX2x7pvIblA9Ksz99TrDYvOa5w=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCvaTdHTWBGv3MKj3KVqJVCw": [
+        "猫又おかゆ",
+        "https://yt3.ggpht.com/a/AATXAJxiB3oU3X4_wTHHAay43njgJgzw-2qKPeYIjDH2=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UChAnqc_AY5_I3Px5dig3X1Q": [
+        "戌神ころね",
+        "https://yt3.ggpht.com/a/AATXAJyvWtmcbmGiE0e1m316wI0MJN6-hGWxjhGZeCWhFA=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCvInZx9h3jC2JzsIzoOebWg": [
+        "不知火フレア",
+        "https://yt3.ggpht.com/a/AATXAJwGmRMcFMvE1JW3v152HnrXfMQxbI_Dw92KH0-CpA=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCdyqAaZDKHXg4Ahi7VENThQ": [
+        "白銀ノエル",
+        "https://yt3.ggpht.com/a/AATXAJyflKVfVjZg11sLPRv8BrLT-8ltdeRcTyKTWlXV=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCCzUftO8KOVkV4wQG1vkUvg": [
+        "宝鐘マリン",
+        "https://yt3.ggpht.com/a/AATXAJwAAoZPNqrR0udm1KMznPYUUTbW-sERt_LZU-af=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UC1DCedRgGHBdm81E1llLhOQ": [
+        "兎田ぺこら",
+        "https://yt3.ggpht.com/a/AATXAJwFs1D2-rT1XPz-fvlO5ZdnkKhPU-Uu6GDwNDn_FA=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCl_gCybOJRIgOXw6Qb4qJzQ": [
+        "潤羽るしあ",
+        "https://yt3.ggpht.com/a/AATXAJwi-Xs0ChOThMuUzFsl3cNZYgdP12Witb0cNSf9=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UC5CwaMl1eIgY8h02uZw7u8A": [
+        "星街すいせい",
+        "https://yt3.ggpht.com/a/AATXAJzbRjSTNu3QQ7pia2yR5oVTcds7MpmeA3a1xE-h=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCZlDXzGoo7d44bwdNObFacg": [
+        "天音かなた",
+        "https://yt3.ggpht.com/a/AATXAJwu4OylyCnOP8gGbO8hINCbwCfPzhBY5JrS0htm=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCS9uQI-jC3DE0L4IpXyvr6w": [
+        "桐生ココ",
+        "https://yt3.ggpht.com/a/AATXAJz_YJU2_OXLiMyWNFnGaC-LpYTs_06G22ozGVfw=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
+    "UCqm3BQLlJfvkTsX_hvm0UmA": [
+        "角巻わため",
+        "https://yt3.ggpht.com/a/AATXAJzqZYR2ukuLZqCDgdsg9eid13borfDPzVBwTIDc=s100-c-k-c0xffffffff-no-rj-mo"
+    ],
     "UC1uv2Oq6kNxgATlCiez59hw": [
         "常闇トワ",
         "https://yt3.ggpht.com/a/AATXAJxqyp7DhLSSrSYRc5HaLcq5QvJvRp3jDnxTeA=s288-c-k-c0xffffffff-no-rj-mo"
@@ -25,7 +116,10 @@ Hololive = {
         "姫森ルーナ",
         "https://yt3.ggpht.com/a/AATXAJzzirDjRJkofWVeoE6gVjodJ0VXaJhy4b_CLg=s288-c-k-c0xffffffff-no-rj-mo"
     ],
-
+    "UCp6993wxpyDPHUpavwDFqgg": [
+        "ときのそら",
+        "https://yt3.ggpht.com/a/AATXAJzGvZJuJ92qM5WcfBcDZqPFSj_CGIEYp9VFmA=s288-c-k-c0xffffffff-no-rj-mo"
+    ],
 } #配信者のチャンネルID, 配信者名, アイコン画像のURLのリスト
 
 webhook_url_Hololive = '配信開始チャンネル用のwebhookリンク' #ホロライブ配信開始
