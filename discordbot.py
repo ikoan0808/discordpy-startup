35b52939a68e35feefcb83f96358e87840671d50
ikoan0808 <71825883+ikoan0808@users.noreply.github.com>
2020-09-25 00:48:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,10 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+# 返信する非同期関数を定義
+async def reply(message):
+    reply = f'{message.author.mention} うんこーーー' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -16,6 +20,14 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('うんこー')
+    
+
+
+# 発言時に実行されるイベントハンドラを定義
+@client.event
+async def on_message(message):
+    if client.user in message.mentions: # 話しかけられたかの判定
+        await reply(message) # 返信する非同期関数を実行
 
 
 bot.run(token)
